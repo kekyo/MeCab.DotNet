ce56bca75f94ecdce97db5ae8f94b0742c97eb5e
Kouji Matsui <k@kekyo.net>
2023-11-22 11:46:49 +0900

    Updated builder script.

--- build-nupkg.sh
+++ build-nupkg.sh
@@ -5,5 +5,5 @@ echo "==========================================================="
 echo "Build MeCab.DotNet"
 echo ""
 
-dotnet build -p:Configuration=Release MeCab.DotNet.sln
-dotnet pack -p:Configuration=Release -o artifacts MeCab.DotNet.sln
+dotnet build -p:Configuration=Release -p:Platform="Any CPU" -p:RestoreNoCache=True MeCab.DotNet.sln
+dotnet pack -p:Configuration=Release -p:Platform="Any CPU" -p:RestoreNoCache=True -o artifacts MeCab.DotNet.sln
